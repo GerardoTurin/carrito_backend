830c86f087a499ca3d2a87dc8c31a3c2aa0186c0
Gerardo Turin <gera_1993_06@hotmail.com>
2024-01-27 18:42:29 -0300

    Carrito back 5 commit

--- DB/configDB.js
+++ DB/configDB.js
@@ -3,9 +3,8 @@ import colors from 'colors';    // Importar módulo colors
 
 
 const conectionDB = async () => {
-    const dbConnection = 'mongodb+srv://gera199306:qIxD0AZdkK7PyvTR@clustercarrito.xvqsdff.mongodb.net/carrito'
     try {
-        await mongoose.connect( dbConnection );
+        await mongoose.connect( process.env.MONGODB_CNN );
         console.log('Base de datos ONLINE'.green);
     } catch (error) {
         console.log(error);
